cd2fa5d8f8c45e7916509bf668175b6d29f242fd
leadgenproMC <leadgenpro10k@gmail.com>
2026-05-06 20:18:23 +0200

    Update Lead type with all properties

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,7 +1,12 @@
 export type Lead = {
+	id?: number
 	name: string
 	phone?: string | null
 	website?: string | null
 	address?: string | null
+	niche?: string
+	location?: string
+	status?: string
+	statusColor?: string
 	source: string
 }
